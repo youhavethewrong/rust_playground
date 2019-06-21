956f862859427f82ed16ceede888335de6a69297
ESC <youhavethewrong@gmail.com>
2019-06-20 20:43:51 -0500

    Generating an informative message when the test doesn't pass.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,10 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,5 +53,15 @@ mod tests {
     #[test]
     fn should_equal_four() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
