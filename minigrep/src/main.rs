c3ca192bf6175b8a2035de6af2ced2f899357c8a
ESC <erickcrager@northwesternmutual.com>
2019-07-01 14:56:25 -0500

    Handle argument errors with Result.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     println!("Main screen turn on.\n");
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("in file {}", config.filename);
@@ -24,9 +28,15 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        }
 
-    Config { query, filename }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
