de6c55286747be1d841f44a473355dbbb90f30ee
ESC <erickcrager@northwesternmutual.com>
2019-07-01 14:31:27 -0500

    Use a config struct for the CLI args.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,18 +5,28 @@ use std::io::prelude::*;
 fn main() {
     println!("Main screen turn on.\n");
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}", config.query);
+    println!("in file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("in file {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong with the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
