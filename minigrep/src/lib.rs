c2abcf4cfa69d8b001610f7468365c6c1980eb0e
ESC <youhavethewrong@gmail.com>
2019-07-06 09:58:21 -0500

    Add tests around run.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -20,7 +20,7 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
@@ -39,11 +39,11 @@ mod test {
     fn should_be_too_few_arguments() {
         let args: Vec<String> = vec![String::from("tacos")];
         let result = Config::new(&args);
-        assert!(result.is_err() && !result.is_ok());
+        assert!(result.is_err());
     }
 
     #[test]
-    fn should_enough_arguments() {
+    fn should_be_enough_arguments() {
         let args: Vec<String> = vec![
             String::from("minigrep"),
             String::from("tacos"),
@@ -59,4 +59,23 @@ mod test {
         assert_eq!("menu.txt", f);
     }
 
+    #[test]
+    fn should_fail_to_find_file() {
+        let config = Config {
+            query: "tacos".to_string(),
+            filename: "a-really-silly-filename-that-should-not-exist.txt".to_string(),
+        };
+        let result = run(config);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn should_find_file() {
+        let config = Config {
+            query: "tacos".to_string(),
+            filename: "Cargo.toml".to_string(),
+        };
+        let result = run(config);
+        assert!(result.is_ok());
+    }
 }
