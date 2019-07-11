642716ed1dcbde7155b8c456dcc135447e1906a8
ESC <youhavethewrong@gmail.com>
2019-07-10 20:12:50 -0500

    Implement search and use it in run.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -26,9 +26,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -77,5 +91,16 @@ mod test {
         };
         let result = run(config);
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,11 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    println!("Main screen turn on.\n");
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("in file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
