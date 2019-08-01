d6af99d28b135ba6b4f4b359add404451cbce531
ESC <erickcrager@northwesternmutual.com>
2019-08-01 15:24:41 -0500

    Refactor into a lib so Cacher can be tested. Cache with a hash map to support inputs with different values.

---
+++ workout_generator/src/lib.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+pub struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: HashMap<u32, u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    pub fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: HashMap::new(),
+        }
+    }
+
+    pub fn value(&mut self, arg: u32) -> u32 {
+        match self.value.get(&arg) {
+            Some(v) => *v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value.insert(arg, v);
+                v
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v1, 1);
+        assert_eq!(v2, 2);
+    }
+}
