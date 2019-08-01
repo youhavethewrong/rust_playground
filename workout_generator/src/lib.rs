fd2c655af732f1a3e50858ac0d21c38ae05830c5
ESC <erickcrager@northwesternmutual.com>
2019-08-01 16:15:37 -0500

    Add generics and trait constraints to support more types.

--- workout_generator/src/lib.rs
+++ workout_generator/src/lib.rs
@@ -1,25 +1,35 @@
+use std::cmp::Eq;
 use std::collections::HashMap;
+use std::hash::Hash;
 
-pub struct Cacher<T>
+pub struct Cacher<T, U, V>
 where
-    T: Fn(u32) -> u32,
+    T: Fn(U) -> V,
+    U: Eq,
+    U: Hash,
+    U: Copy,
+    V: Copy,
 {
     calculation: T,
-    value: HashMap<u32, u32>,
+    value: HashMap<U, V>,
 }
 
-impl<T> Cacher<T>
+impl<T, U, V> Cacher<T, U, V>
 where
-    T: Fn(u32) -> u32,
+    T: Fn(U) -> V,
+    U: Eq,
+    U: Hash,
+    U: Copy,
+    V: Copy,
 {
-    pub fn new(calculation: T) -> Cacher<T> {
+    pub fn new(calculation: T) -> Cacher<T, U, V> {
         Cacher {
             calculation,
             value: HashMap::new(),
         }
     }
 
-    pub fn value(&mut self, arg: u32) -> u32 {
+    pub fn value(&mut self, arg: U) -> V {
         match self.value.get(&arg) {
             Some(v) => *v,
             None => {
@@ -36,13 +46,18 @@ mod test {
     use super::*;
 
     #[test]
-    fn call_with_different_values() {
-        let mut c = Cacher::new(|a| a);
-
+    fn can_cache_different_values() {
+        let mut c = Cacher::new(|a: u32| a);
         let v1 = c.value(1);
         let v2 = c.value(2);
-
         assert_eq!(v1, 1);
         assert_eq!(v2, 2);
+    }
+
+    #[test]
+    fn can_cache_more_than_numbers() {
+        let mut c = Cacher::new(|a: &str| a);
+        let v = c.value("Pizza");
+        assert_eq!(v, "Pizza");
     }
 }
