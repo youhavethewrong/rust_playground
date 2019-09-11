b7c28ec7267c833766b8799d08a31ff5189386b7
ESC <youhavethewrong@gmail.com>
2019-09-11 13:59:27 -0500

    Following along with Ch 15. Want to play with syscalls, but not on macOS.

--- junk/src/main.rs
+++ junk/src/main.rs
@@ -3,10 +3,13 @@ extern crate junk;
 fn main() {
     println!("Main screen turn on!");
 
+    let x = Box::new(27);
+
     let v1: Vec<i32> = vec![1, 2, 3];
     let v1_iter = v1.iter().map(|x| x + 1);
 
     for val in v1_iter {
         println!("Got: {}", val);
     }
+    println!("Woah I have {} in a Box!", x);
 }
