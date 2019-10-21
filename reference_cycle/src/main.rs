a28de876c30148f3bc9a09eb900551e82d9df483
ESC <youhavethewrong@gmail.com>
2019-10-21 13:04:58 -0500

    Switching to a Weak reference prevents a reference cycle.

--- reference_cycle/src/main.rs
+++ reference_cycle/src/main.rs
@@ -1,11 +1,12 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 use List::{Cons, Nil};
 
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -49,13 +50,20 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
     println!("branch dump: {:?}", branch);
 }
