cd7748fa0f56191b264d25a3a651712fed77bdd5
ESC <youhavethewrong@gmail.com>
2019-11-05 11:22:10 -0600

    Use move to move ownership into the closure.

--- concurrent/src/main.rs
+++ concurrent/src/main.rs
@@ -15,4 +15,12 @@ fn main() {
     }
 
     handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let second_handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    second_handle.join().unwrap();
 }
