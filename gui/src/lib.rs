94b968833e2d651bd53117b1a86b052f59ab345f
ESC <youhavethewrong@gmail.com>
2019-12-27 09:29:37 -0600

    Use the Draw trait to implement a select box.

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -14,6 +14,18 @@ impl Screen {
     }
 }
 
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
