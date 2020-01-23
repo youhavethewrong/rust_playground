71f64bfa4f47ff62f24df62cddcdb2e421d7cd5c
ESC <youhavethewrong@gmail.com>
2020-01-23 07:11:43 -0600

    Simple bag with IntoIterator support.

--- bqs/src/lib.rs
+++ bqs/src/lib.rs
@@ -1,4 +1,4 @@
-pub struct Item {
+pub struct Marble {
     pub name: &'static str,
     pub value: i32,
 }
@@ -21,31 +21,59 @@ impl<T> Bag<T> {
     }
 }
 
+impl<T> IntoIterator for Bag<T> {
+    type Item = T;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.contents.into_iter()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn make_empty_bag() {
-        let contents: Vec<Box<Item>> = vec![];
+        let contents: Vec<Box<Marble>> = vec![];
         let bag = Bag { contents };
         assert_eq!(0, bag.size());
         assert_eq!(true, bag.is_empty());
     }
 
     #[test]
-    fn add_one_item() {
-        let contents: Vec<Box<Item>> = vec![];
+    fn add_one_marble() {
+        let contents: Vec<Box<Marble>> = vec![];
         let mut bag = Bag { contents };
         assert_eq!(0, bag.size());
         assert_eq!(true, bag.is_empty());
 
-        let item = Item {
+        let marble = Marble {
             name: "ok",
             value: 77,
         };
-        bag.add(Box::from(item));
+        bag.add(Box::from(marble));
         assert_eq!(1, bag.size());
         assert_eq!(false, bag.is_empty());
+    }
+
+    #[test]
+    fn value_sum() {
+        let contents: Vec<Box<Marble>> = vec![];
+        let mut bag = Bag { contents };
+        let marble_a = Marble {
+            name: "ok",
+            value: 99,
+        };
+
+        let marble_b = Marble {
+            name: "ok",
+            value: 1,
+        };
+
+        bag.add(Box::from(marble_a));
+        bag.add(Box::from(marble_b));
+        assert_eq!(100, bag.into_iter().fold(0, |acc, m| acc + m.value))
     }
 }
